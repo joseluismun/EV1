52457b23c8b5c41cf3d949224bf742d3dbb47506
Jose Luis Muñoz <jl.munozp@duocuc.cl>
2023-04-14 18:12:26 +0000

    Add files via upload

---
+++ script.py
@@ -0,0 +1,17 @@
+print("Que nota necesitas sacarte")
+
+codigo = int(input("Ingrese su codigo de curso (solo numeros): "))
+cantidad = int(input("Ingrese su cantidad de notas: "))
+n1 = float(input("Ingrese nota parcial 1: "))
+n2 = float(input("Ingrese nota parcial 2: "))
+nn = float(input("Ingrese nota parcial N: "))
+valor = int(input("¿Cuanto vale el examen en porcentaje?: "))
+
+print("Tu promedio es: La suma de",n1,"+",n2,"+",nn,"y dividido en dos, es igual a",(n1+n2+nn)/3)
+
+
+
+
+
+
+
